084eeff7fb953bdfd8da93adf8393da069540822
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2023-12-25 15:40:51 +0700

    finish exercise 2+3

--- react-1/exercise-2/src/App.jsx
+++ react-1/exercise-2/src/App.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import "../src/assets/style.css";
 
 function App() {
   return (
@@ -12,29 +13,36 @@ function App() {
 
 function Header() {
   return (
-    // Code for Header
-    // <Header />
+    <header>
+      <span>Turn on / off</span>
+      <p>Current Temperature: XXX</p>
+    </header>
   );
 }
 
 function Content() {
   return (
-    // Code for Content
-    // <Content />
+    <main>
+      <Temperature />
+    </main>
   );
 }
 
 function Temperature() {
+  let temperature = "30";
   return (
-    // Code for Temperature
-    // <Temperature />
+    <div id="temperature">
+      <span>{temperature} Oc</span>
+    </div>
   );
 }
 
 function Footer() {
   return (
-    // Code for Footer
-    // <Footer />
+    <footer>
+      <button>Up</button>
+      <button>Down</button>
+    </footer>
   );
 }
 
--- react-1/exercise-3/src/App.jsx
+++ react-1/exercise-3/src/App.jsx
@@ -1,22 +1,52 @@
 import React from "react";
+import "../src/assets/style.css";
 import "./App.css";
 
 function App() {
-  return <div id="app">
-    <RegistrationForm />
-    <DisplayMember />
-  </div>;
+  return (
+    <div id="app">
+      <RegistrationForm />
+      <DisplayMember />
+    </div>
+  );
 }
 
 const RegistrationForm = () => {
   return (
-    // Code here
+    <div className="container">
+      <h1>Registration Form</h1>
+      <form id="registrationForm">
+        <FormGroup />
+      </form>
+    </div>
+  );
+};
+
+const FormGroup = () => {
+  return (
+    <div className="form-group">
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" name="name" required />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input type="email" id="email" name="email" required />
+      </div>
+      <button type="submit">Register</button>
+    </div>
   );
 };
 
 const DisplayMember = () => {
+  let johnDoe = "John Doe (john@example.com)";
+  let janeSmith = "Jane Smith (jane@example.com)";
   return (
-    // Code here
+    <div className="members">
+      <h2>Registered Members</h2>
+      <div className="member">{johnDoe}</div>
+      <div className="member">{janeSmith}</div>
+    </div>
   );
 };
 
