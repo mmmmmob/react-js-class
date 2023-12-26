d1bd523cc1c3db0fe33bd88c27ea3eb81e9c29b4
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2023-12-26 13:58:52 +0700

    finish react-2 exercise 2

--- react-2/exercise-2/src/App.jsx
+++ react-2/exercise-2/src/App.jsx
@@ -1,9 +1,9 @@
+import React from "react";
 import "./App.css";
 
 function App() {
   return (
     <div id="app">
-      <h1>Enter Data</h1>
       <PostContainer />
       <FeedSection />
     </div>
@@ -12,19 +12,23 @@ function App() {
 
 const PostContainer = () => {
   return (
-    <div class="post-container">
-      <div class="post-header">
-        <img class="post-avatar" src="avatar.jpg" alt="Your Avatar" />
-        <div class="post-author">You</div>
+    <div className="post-container">
+      <div className="post-header">
+        <img
+          src="../html/avatar.jpg"
+          alt="Your Avatar"
+          className="post-avatar"
+        />
+        <div className="post-author">You</div>
       </div>
-      <div class="post-content">
+      <div className="post-content">
         <textarea
-          class="post-input"
+          className="post-input"
           placeholder="What's on your mind?"
         ></textarea>
       </div>
-      <div class="post-actions">
-        <button class="post-button">Post</button>
+      <div className="post-action">
+        <button className="post-button">Post</button>
       </div>
     </div>
   );
@@ -32,65 +36,63 @@ const PostContainer = () => {
 
 const FeedSection = () => {
   return (
-    <div class="feed">
-      <div class="post">
-        <div class="post-header">
-          <img
-            class="post-avatar"
-            src="https://cdn.pixabay.com/photo/2017/09/01/00/15/png-2702691_640.png"
-            alt="User 1"
-          />
-          <div>
-            <div class="post-author">User 1</div>
-            <div class="post-time">2 hours ago</div>
+    <div className="feedHome">
+      <div className="feed">
+        <div className="post">
+          <div className="post-header">
+            <img
+              src="https://cdn.pixabay.com/photo/2017/09/01/00/15/png-2702691_640.png"
+              alt="User 1"
+              className="post-avatar"
+            />
+            <div className="post-author">User 1</div>
+            <div className="post-time">2 hours ago</div>
           </div>
+          <div className="post-content">This is my first post! 🎉</div>
         </div>
-        <div class="post-content">This is my first post! 🎉</div>
         <img
-          class="post-image"
           src="https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png"
           alt="Post 1"
         />
-      </div>
-
-      <div class="post">
-        <div class="post-header">
-          <img
-            class="post-avatar"
-            src="https://images.rawpixel.com/image_png_social_square/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvcm00NTYtMDA3YS5wbmc.png"
-            alt="User 2"
-          />
-          <div>
-            <div class="post-author">User 2</div>
-            <div class="post-time">4 hours ago</div>
+        <button>DELETE</button>
+        <div className="post">
+          <div className="post-header">
+            <img
+              src="https://images.rawpixel.com/image_png_social_square/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvcm00NTYtMDA3YS5wbmc.png"
+              alt="User 2"
+              className="post-avatar"
+            />
+            <div className="post-author">User 2</div>
+            <div className="post-time">4 hours ago</div>
+          </div>
+          <div className="post-content">
+            Enjoying a beautiful day outdoors! ☀️
           </div>
         </div>
-        <div class="post-content">Enjoying a beautiful day outdoors! ☀️</div>
         <img
-          class="post-image"
           src="https://images.rawpixel.com/image_png_social_square/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvcm00NTYtMDA3YS5wbmc.png"
           alt="Post 2"
         />
-      </div>
-
-      <div class="post">
-        <div class="post-header">
-          <img
-            class="post-avatar"
-            src="https://images.rawpixel.com/image_png_social_square/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvcm00NTYtMDA3YS5wbmc.png"
-            alt="User 3"
-          />
-          <div>
-            <div class="post-author">User 3</div>
-            <div class="post-time">1 day ago</div>
+        <button>DELETE</button>
+        <div className="post">
+          <div className="post-header">
+            <img
+              src="https://images.rawpixel.com/image_png_social_square/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvcm00NTYtMDA3YS5wbmc.png"
+              alt="User 3"
+              className="post-avatar"
+            />
+            <div className="post-author">User 3</div>
+            <div className="post-time">1 day ago</div>
+          </div>
+          <div className="post-content">
+            Exploring new places and cultures. 🌍✈️
           </div>
         </div>
-        <div class="post-content">Exploring new places and cultures. 🌍✈️</div>
         <img
-          class="post-image"
           src="https://images.rawpixel.com/image_png_social_square/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvcm00NTYtMDA3YS5wbmc.png"
           alt="Post 3"
         />
+        <button>DELETE</button>
       </div>
     </div>
   );
