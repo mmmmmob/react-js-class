384097cd19b6bfc17741c6121a39c148747f0d7c
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2023-12-28 10:13:56 +0700

    finish react-3 exercise 4 and comment

--- react-3/exercise-4/src/App.jsx
+++ react-3/exercise-4/src/App.jsx
@@ -1,6 +1,7 @@
-import usePost from "./hook/usePost";
-import { getUser } from "./hook/me";
+import { useState } from "react";
 import "./App.css";
+import { getUser } from "./hook/me";
+import usePost from "./hook/usePost";
 
 function App() {
   const { get, remove, update, create } = usePost();
@@ -10,34 +11,74 @@ function App() {
     let id = `id-${Math.floor(Math.random() * 10000)}`; // generate id here by Math.random() (please use integer)
     let time = new Date().toDateString(); // generate timestamp here by (new Date().toDateString())
     const user = getUser();
-    let data = {};
+    // create data object according to api document that require 6 key data
+    let data = {
+      id: id,
+      // avatar and author from getUser() (also stored in object type so we can call with [obj.key] template)
+      avatar: user.avatar,
+      author: user.author,
+      time: time,
+      content: content,
+      image: image,
+    };
     create(data);
   };
 
   return (
     <div id="app">
       <h1>Enter Data</h1>
-      <PostContainer />
+      <PostContainer createPost={createPost} />
       <FeedSection posts={posts} removeHandler={remove} />
     </div>
   );
 }
 
-const PostContainer = () => {
+const PostContainer = (props) => {
+  // create props connection to App()
+  const createPost = props.createPost;
+  // create stateValue and stateMethod for content and image according to mental model
+  const [content, setContent] = useState();
+  const [image, setImage] = useState();
+  // create function to receive content and image from text field according to mental model
+  const createPostInPostContainer = () => {
+    createPost(content, image);
+  };
   return (
     <div className="post-container">
       <div className="post-header">
         <img className="post-avatar" src="avatar.jpg" alt="Your Avatar" />
         <div className="post-author">You</div>
       </div>
+
       <div className="post-content">
         <textarea
           className="post-input"
           placeholder="What's on your mind?"
+          // use onChange to focus on value input from user
+          onChange={(e) => {
+            setContent(e.target.value);
+          }}
+        ></textarea>
+        <textarea
+          className="image-input"
+          placeholder="Enter image's url"
+          // use onChange to focus on value input from user
+          onChange={(e) => {
+            setImage(e.target.value);
+          }}
         ></textarea>
       </div>
+
       <div className="post-actions">
-        <button className="post-button">Post</button>
+        <button
+          className="post-button"
+          // use onClick to trigger function
+          onClick={() => {
+            createPostInPostContainer();
+          }}
+        >
+          Post
+        </button>
       </div>
     </div>
   );
