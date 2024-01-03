fa4eae7c33f24ec6615aa9dbd06779b38ccf6d75
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-01-03 15:50:57 +0700

    make a change to exercise 1 at single data by id

--- react-5/exercise-1/src/App.jsx
+++ react-5/exercise-1/src/App.jsx
@@ -1,11 +1,10 @@
-import { useEffect, useState } from "react";
 import axios from "axios";
-import Card from "./Card";
+import { useEffect, useState } from "react";
 import "./App.css";
+import Card from "./Card";
 
 const App = () => {
   const [members, setMembers] = useState([]);
-
   const [singleMember, setSingleMember] = useState({});
 
   useEffect(() => {
@@ -13,7 +12,6 @@ const App = () => {
       const response = await axios.get(
         "https://jsd5-mock-backend.onrender.com/members"
       );
-      // set member here
       if (response.status === 200 && response.data) {
         setMembers([...response.data]);
       }
@@ -23,13 +21,11 @@ const App = () => {
   }, []);
 
   const getDataById = async (id) => {
-    // call axios here
     const response = await axios.get(
       `https://jsd5-mock-backend.onrender.com/members/${id}`
     );
 
     if (response.status === 200 && response.data) {
-      // set data here
       const { data } = response;
       setSingleMember({ ...data });
     }
@@ -52,19 +48,9 @@ const App = () => {
       <h1>Single Data by ID</h1>
       <div>
         <select onChange={(ev) => getDataById(ev.target.value)}>
-          <option value="cc9754c4-f619-4921-bf7e-8a4fc9714e7f">
-            Mock Name
-          </option>
-          <option value="66a3f853-5c09-4412-a380-3cc1ebf08c9c">John Doe</option>
-          <option value="64715e05-b45b-4aba-b6d2-d7a4f61ccf0a">
-            Alice Smith
-          </option>
-          <option value="61ec7ef2-f2e5-460c-bfd0-c060a99d0f50">
-            Jane Johnson
-          </option>
-          <option value="6b72fbe1-5c72-42bb-a25f-8d00cef7e88a">
-            Bob Brown
-          </option>
+          {members.map((member) => (
+            <option value={member.id}>{member.name}</option>
+          ))}
         </select>
         <Card
           id={singleMember.id}
