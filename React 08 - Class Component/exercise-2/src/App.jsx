fb592ba682419da0f0f6234da95a7cd57678d495
Theppitak M. <141404845+mmmmmob@users.noreply.github.com>
2024-01-05 13:37:54 +0700

    finisg Legacy class component exercise 2

--- React 08 - Class Component/exercise-2/src/App.jsx
+++ React 08 - Class Component/exercise-2/src/App.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import "./App.css";
 
+// Legacy React class : Props (translated from ./App-function.jsx)
+
 class App extends React.Component {
-  // members is ARRAY containing OBJECT.
   members = [
     {
       name: "Mock J",
@@ -53,16 +54,80 @@ class App extends React.Component {
       <div id="app">
         <h1>Enter Data</h1>
         <RunningForm />
-        <TableDisplay memberData={members} />
+        {/* set a props to variable with {this.__[variable name in class]__} */}
+        <TableDisplay memberData={this.members} />
       </div>
     );
   }
 }
 
 class RunningForm extends React.Component {
-  // code here
+  render() {
+    return (
+      <form id="data-form">
+        <label htmlFor="name">Name:</label>
+        <input type="text" id="name" name="name" />
+        <br />
+        <br />
+
+        <label htmlFor="age">Age:</label>
+        <input type="number" id="age" name="age" />
+        <br />
+        <br />
+
+        <label htmlFor="weight">Weight:</label>
+        <input type="number" id="weight" name="weight" />
+        <br />
+        <br />
+
+        <label htmlFor="running">Current Running (Minutes):</label>
+        <input type="number" id="running" name="running" />
+        <br />
+        <br />
+
+        <button type="button">Add Data</button>
+      </form>
+    );
+  }
 }
 
-// class of TableDisplay here
+class TableDisplay extends React.Component {
+  // ln:95-96 : set a constructor command
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <>
+        <h1>Entered Data</h1>
+        <table id="data-table" border="1" width="100%">
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Age</th>
+              <th>Weight</th>
+              <th>Current Running (Minutes)</th>
+              <th>Good Running</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* use the data sent via props with this.props.__[variable name set at parent class]__ */}
+            {this.props.memberData.map((item) => {
+              return (
+                <tr>
+                  <td>{item.name}</td>
+                  <td>{item.age}</td>
+                  <td>{item.weight}</td>
+                  <td>{item.running}</td>
+                  <td>{item.status}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </>
+    );
+  }
+}
 
 export default App;
